adb3dbb2983879fa18cd3a079497ed1fe5cc6de1
mith83 <63154565+mith83@users.noreply.github.com>
2025-01-19 21:25:30 +0530

    Update share.js
    best one

--- share.js
+++ share.js
@@ -1,34 +1,22 @@
 function webShareAPI(header, description, link, imgfiles = "") {
-    if (typeof AndroidShare !== "undefined" && AndroidShare.shareContent) {
-        if (imgfiles == "") {
-            AndroidShare.shareContent(header, description, link);
-            console.log("Successful share");
-        }
-        else {
-            var shareData = { files: imgfiles };
-            AndroidShare.shareContent(header, description, link, shareData);
-            console.log("Successful share");
-        }
-
-
+  if (imgfiles == "") {
+    if (navigator.share) {
+      navigator.share({title: header,text: description,url: link,})
+      .then(() => console.log("Successful share"))
+      .catch((error) => console.log("Error sharing", error));
     } else {
-        if (imgfiles == "") {
-            if (navigator.share) {
-                navigator.share({
-                    title: header,
-                    text: description,
-                    url: link,
-                })
-                    .then(() => console.log("Successful share"))
-                    .catch((error) => console.log("Error sharing", error));
-            }
-        } else {
-
-            if (navigator.canShare && navigator.canShare(shareData)) {
-                navigator.share({ title: header, text: description, url: link, files: imgfiles })
-                    .then(() => console.log("Successful share"))
-                    .catch((error) => console.log("Error sharing", error));
-            }
-        }   
+      AndroidShare.shareContent(header, description, link);
+      console.log("Successful share");
     }
+  } else {
+    var shareData = { files: imgfiles };
+    if (navigator.canShare && navigator.canShare(shareData)) {
+      navigator.share({ title: header, text: description, url: link, files: imgfiles })
+      .then(() => console.log("Successful share"))
+      .catch((error) => console.log("Error sharing", error));
+    } else {
+      AndroidShare.shareContent(header, description, link, shareData);
+      console.log("Successful share");
+    }
+  } 
 }
